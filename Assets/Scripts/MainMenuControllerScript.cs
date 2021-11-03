2dbe2dffdaae72e23784bf81f06e1b8071854231
Sai <thegeekskm101@gmail.com>
2021-11-03 13:34:21 -0500

    Main Menu Done
    The main menu has a play button and an exit button. maybe add support for settings/about button..?

---
+++ Assets/Scripts/MainMenuControllerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuControllerScript : MonoBehaviour
+{
+    #region Variables
+
+
+
+    #endregion
+
+    public void ExitTheGame()
+    {
+        Application.Quit();
+    }
+
+    public void GoToGame()
+    {
+        //have to make sure that the build index for the actual game scene is always 1 + the buildIndex of the main menu scene.
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
