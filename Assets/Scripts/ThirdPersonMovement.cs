1986804c5aee2769dd92f5f656584a13a0a31f62
Sai <thegeekskm101@gmail.com>
2021-11-04 21:00:15 -0500

    Adding RPG Elements Part 1

---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    [Header("Connections")]
+    [SerializeField] CharacterController controller;
+    [SerializeField] Transform cam;
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundDistance = 0.4f;
+    [SerializeField] LayerMask groundMask;
+
+
+   
+
+    [Header("Player Attributes")]
+    [SerializeField] public float speed = 6f;
+    [SerializeField] float turnSmoothTime = 0.1f;
+    [SerializeField] float gravity = -9.81f;
+    [SerializeField] public float jumpHeight = 3f;
+    [SerializeField] public float additionalSprintSpeed;
+
+    float turnSmoothVelocity;
+    Vector3 currentVelocity;
+    bool isGrounded;
+    bool isSprinting;
+
+
+   
+
+    void Update()
+    {
+        if (transform.position.y < 0)
+        {
+            transform.position = new Vector3(0, 45f, 0);
+        }
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && currentVelocity.y < 0)
+        {
+            currentVelocity.y = -2f;
+        }
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+       
+
+       if (isSprinting)
+        {
+            controller.Move(transform.forward * (speed + additionalSprintSpeed) * Time.deltaTime);
+        }
+        else
+        {
+            controller.Move(transform.forward * speed * Time.deltaTime);
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            currentVelocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        currentVelocity.y += gravity * Time.deltaTime;
+        controller.Move(currentVelocity * Time.deltaTime);
+    }
+}
