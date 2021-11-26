ac751fef6a86dc5b5638371ad3c0b4e831fe9436
Sai <thegeekskm101@gmail.com>
2021-11-25 22:05:27 -0600

    Rooms Created
    The Level Design is complete. Now all that is left to do is create collectibles, enemies, UI, and yeah

--- Assets/Scripts/ThirdPersonMovement.cs
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -1,17 +1,8 @@
-﻿using UnityEngine;
+using UnityEngine;
 
 public class ThirdPersonMovement : MonoBehaviour
 {
-    [Header("Connections")]
-    [SerializeField] CharacterController controller;
-    [SerializeField] Transform cam;
-    [SerializeField] Transform groundCheck;
-    [SerializeField] float groundDistance = 0.4f;
-    [SerializeField] LayerMask groundMask;
-
-
-   
-
+    
     [Header("Player Attributes")]
     [SerializeField] public float speed = 6f;
     [SerializeField] float turnSmoothTime = 0.1f;
@@ -19,7 +10,7 @@ public class ThirdPersonMovement : MonoBehaviour
     [SerializeField] public float jumpHeight = 3f;
     [SerializeField] public float additionalSprintSpeed;
 
-    float turnSmoothVelocity;
+
     Vector3 currentVelocity;
     bool isGrounded;
     bool isSprinting;
@@ -29,39 +20,8 @@ public class ThirdPersonMovement : MonoBehaviour
 
     void Update()
     {
-        if (transform.position.y < 0)
-        {
-            transform.position = new Vector3(0, 45f, 0);
-        }
-
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-
-        if (isGrounded && currentVelocity.y < 0)
-        {
-            currentVelocity.y = -2f;
-        }
-
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
-
-       
-
-       if (isSprinting)
-        {
-            controller.Move(transform.forward * (speed + additionalSprintSpeed) * Time.deltaTime);
-        }
-        else
-        {
-            controller.Move(transform.forward * speed * Time.deltaTime);
-        }
-
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            currentVelocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-        }
-
-        currentVelocity.y += gravity * Time.deltaTime;
-        controller.Move(currentVelocity * Time.deltaTime);
+        
     }
 }
